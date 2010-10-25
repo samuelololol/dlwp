3dfb3826a0627123734cdcbc802896864c5dfc1c
Samuel <samuelololol@gmail.com>
2010-10-25 23:48:52 +0800

    change main entry

--- py/test_sid.py
+++ py/test_sid.py
@@ -13,9 +13,13 @@ from dl_img import download_img
 ty_img = []
 kon_img = []
 
-#kon_re_func(kon_img) 
-print kon_img
+def main():
+    kon_re_func(kon_img) 
+    print kon_img
 
-#ty_re_func(ty_img) 
-print ty_img
+    ty_re_func(ty_img) 
+    print ty_img
 
+
+if __name__ == '__main__':
+    main()
