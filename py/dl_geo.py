cb98be44487414ac600946263ea122aafc32f34b
Samuel <samuelololol@gmail.com>
2010-12-10 12:59:59 +0800

    add nationalgeographic photo of the day

---
+++ py/dl_geo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from dl_page import get_raw_page
+from dl_img import download_img
+
+geo_page = []
+geo_dict = dict()
+geo_source_url = 'http://photography.nationalgeographic.com/photography/photo-of-the-day'
+geo_re = re.compile(r"""
+         (download_link)        # start of 'similarhr' or 'similarlr'
+         (.)+                   
+         (</div>\r)  
+         """, re.VERBOSE)
+
+def geo_re_func(img):
+    subname_re = re.compile(r"""
+            [\/]
+            """, re.VERBOSE)
+    geo_page = get_raw_page(geo_source_url) 
+    outiter = geo_re.finditer(geo_page)
+    for out in outiter:
+        link = out.group()[24:-52]
+        filename = subname_re.split(link)
+        file = filename[9]
+        download_img(link, file)
+        #print file, link
+        img.append(link)
+
+        #geo_dict[link] = file
+        #print geo_dict
+    return 
--- py/test_sid.py
+++ py/test_sid.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#
-#
-#
-#
 import os
 from dl_kon import kon_re_func
 from dl_ty  import ty_re_func
+from dl_geo import geo_re_func
 from dl_img import download_img
 
 ty_img = []
 kon_img = []
+geo_img = []
 
 def main():
-    kon_re_func(kon_img) 
-    print kon_img
+    #kon_re_func(kon_img) 
+    #print kon_img
 
-    ty_re_func(ty_img) 
-    print ty_img
+    #ty_re_func(ty_img) 
+    #print ty_img
+
+    geo_re_func(geo_img)
+    print geo_img
+
 
 
 if __name__ == '__main__':
