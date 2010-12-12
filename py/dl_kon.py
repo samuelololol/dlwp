ceae0acc849a605fadecd0b6e075e03e68cd7936
Samuel <samuelololol@gmail.com>
2010-12-13 02:01:59 +0800

    fixing the kon, not done yet

--- py/dl_kon.py
+++ py/dl_kon.py
@@ -7,9 +7,10 @@ kon_page = []
 kon_dict = dict()
 kon_source_url = 'http://konachan.com/post/popular_recent?period=1d'
 kon_re = re.compile(r"""
-         (similarhr|similarlr)  # start of 'similarhr' or 'similarlr'
-         (.)+                   # else
-         (<span>)               # trailing span
+         (directlink\ largeimg) 
+         (.)+
+         (<span\ class)  
+         (.)+
          """, re.VERBOSE)
 
 def kon_re_func(img):
@@ -19,9 +20,10 @@ def kon_re_func(img):
     kon_page = get_raw_page(kon_source_url) 
     outiter = kon_re.finditer(kon_page)
     for out in outiter:
-        link = out.group()[17:-8]
-        filename = subname_re.split(link)
-        file = filename[18][2:] + '.' + filename[-1]
+        link = out.group()
+        print link[0]
+        #filename = subname_re.split(link)
+        #file = filename[18][2:] + '.' + filename[-1]
         #download_img(link, file)
         #print file, link
         img.append(link)
---
+++ py/index.php
@@ -0,0 +1,264 @@
+<HTML>
+<HEAD><TITLE>wxPython</TITLE>
+</HEAD>
+<BODY BGCOLOR="#FFFFFF">
+<div align="center">
+<a href="index.php" target="_top"><img src="images/wxPython.jpg" border=0></a></div><hr><p>
+
+<table width="100%" border="0" cellspacing="0" cellpadding="4">
+<tr>
+<td width="180" valign="top">
+<!-- BEGIN Navigation -->
+<table border="1" cellspacing="0" cellpadding="0">
+<tr><td>
+
+<br>
+<table border="0" cellspacing="0" cellpadding="0"><tr><td colspan="2">
+
+<a href="index.php" target="_top"><img src="images/home_sm.jpg" width="75" height="30" alt="Home" border="0" hspace="0" vspace="0" align="top"></a></td></tr>
+<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><font size="-1">
+	<li><a href="what.php" target="_top">Introduction</a>
+	<li><a href="quotes.php" target="_top">Quotes</a>
+	<li><a href="screenshots.php" target="_top">Screen shots</a>
+	<li><a href="http://wiki.wxpython.org/index.cgi/How_to_Learn_wxPython" target="_top">Learning wxPython</a>
+	<li><a href="recentchanges.php" target="_top">Recent Changes</a>
+<!--	<li><a href="migrationguide.php" target="_top">Migration Guide</a>
+	<li><a href="py.php" target="_top">Slice of Py</a>  -->
+	<li><a href="onlinedocs.php" target="_top">Online wxDocs</a>
+	<li><a href="docs/api/" target="_top">New wxPyDocs</a> 
+	<li><a href="presentations.php" target="_top">Presentations</a>
+	<li><a href="http://www.manning.com/affiliate/idevaffiliate.php?id=189_23" target="_top">Buy the Book</a>
+        <li><a href="bookerrata.php" target="_top">Book Errata</a>
+        <li><a href="http://www.cafepress.com/wxpython" target="_top">Get the T-shirt</a>
+	<li><a href="searchsite.php" target="_top">Search this site</a>
+	<!-- <li><a href="http://wxPROs.com/" target="_top">Commercial support</a> -->
+
+<p><br></font></td></tr>
+
+<tr><td colspan="2">
+<a href="download.php" target="_top"><img src="images/download_sm.jpg" width="132" height="31" alt="download" border="0" hspace="0" vspace="0"></a></td></tr>
+<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><font size="-1">
+	<li><a href="download.php#stable" target="_top">Stable</a>
+	<li><a href="download.php#unstable" target="_top">Development</a>
+	<!-- <li><a href="download.php#svnaccess" target="_top">SVN access</a> -->
+	<li><a href="builddoc.php" target="_top">Build instructions</a>
+
+<p><br></font></td></tr>
+
+<tr><td colspan="2">
+<img src="images/community_sm.jpg" width="141" height="39" alt="" border="0" hspace="0" vspace="0"></td></tr>
+<tr><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+<td><font size="-1">
+	<li><a href="http://wiki.wxpython.org/" target="_top">wxPyWiki</a>
+	<li><a href="http://wxpython.org/blog/" target="_top">wxForty-Two</a>
+	<li><a href="maillist.php" target="_top">Mail lists</a>
+ 	<li><a href="http://svn.wxwidgets.org/viewvc" target="_top">Browse sources</a>
+	<li><a href="http://trac.wxwidgets.org/" target="_top">Report a bug</a>
+	<li><a href="http://trac.wxwidgets.org/" target="_top">Submit a patch</a>
+	<li><a href="http://trac.wxwidgets.org/" target="_top">Feature Request</a>
+	<li><a href="contribute.php" target="_top">Contribute</a>
+	<li><a href="purchase.php" target="_top">Support</a>
+	<li><a href="codeguidelines.php" target="_top">Code guidelines</a>
+	<!-- <li><a href="users.php" target="_top">Users</a>
+	<li><a href="samples.php" target="_top">Snippets and Samples</a> -->
+
+</font>
+<!--
+<p><br>
+<a href="http://www.amazon.com/gp/product/1932394621?ie=UTF8&tag=wxpython-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1932394621"><img
+src="images/wxPyBook_cover_120.png"
+width=120 height=149 border="0" alt="wxPython in Action" align="left"></a>
+-->
+<!-- http://ec1.images-amazon.com/images/P/1932394621.01._SCTHUMBZZZ_.jpg -->
+<p>
+</td></tr>
+</table>
+
+
+</td></tr>
+</table>
+<!-- END Navigation -->
+
+</td>
+<td align="left" valign="top">
+
+
+
+<p>
+
+<a name="top"></a>
+
+Welcome to the home of wxPython, a blending of the
+<a href="http://wxwidgets.org/" target="_top">wxWidgets</a> C++ class library  with the <a href="http://www.python.org/" target="_top">Python</a> programming language.
+
+<p>
+<br>
+<br>
+
+
+
+<table width="100%" border="1" cellspacing="0" cellpadding="8" bgcolor="#BCB8E7">
+<tr><td>
+<div align="center"><font size="+3" style="font-family: sans-serif;"><b>News</b></font></div>
+<p>
+<ul>
+<li><i>(9-Dec-2010)</i> <b>wxPython 2.8 Application Development Cookbook</b> by Cody Precord is published and <a href="#wxPyCookbook">available</a> for purchase.</li><p>
+
+<li><i>(19-October-2010)</i> <b>wxPython 2.9.1.1</b> The first public release of wxPython from the 2.9 development <a href="http://wiki.wxpython.org/ReleaseSeries">release series</a>, with lots and lots (and lots) of changes, enhancements and fixes.  Details are in the <a href="recentchanges.php">Recent Changes</a> document.</li><p>
+
+<li><i>(14-May-2010)</i> <b>wxPython 2.8.11.0</b> Adds Python 2.7 builds, PySlices, new pubsub implementation, lots of updates to AGW, and lots of bugs fixed.  </li><p>
+
+<li><i>(22-May-2009)</i> The wx and wxPython mail lists are moving to Google Groups.  See the <a href="maillist.php">mail list page</a> for the new addresses and links.</li><p>
+
+<li><i>(14-May-2009)</i> <b>wxPython 2.8.10.1</b> This release fixes the problem with using Python 2.6's default manifest, and updates wxcairo to work with the latest PyCairo.  </li><p>
+
+<li><i>(16-Feb-2009)</i> <b>wxPython 2.8.9.2</b> This release adds the wx.lib.agw package, adds an event watcher to the widget inspection tool, and fixes a bunch of bugs.  </li><p>
+
+
+<li><i>(8-Oct-2008)</i> A set of wxPython binaries for <b>Python 2.6</b> on the Mac and Windows (32-bit and 64-bit) are now available.</li><p>
+
+<li><i>(29-Sept-2008)</i> <b>wxPython 2.8.9.1</b> is now <a href="download.php">available.</a> This release fixes a compatibility problem with Python 2.4.</li><p>
+
+<li><i>(28-Sept-2008)</i> <b>wxPython 2.8.9.0</b> This release adds support for using Cairo for drawing on wx windows, adds a Win64 build, and various other fixes and enhancements. 
+</li><p>
+
+<li><i>(18-July-2008)</i> <b>wxPython 2.8.8.1</b> This is a minor bugfix release with several fixes for problems discovered in the 2.8.8.0 release.
+<p>
+
+
+<li><i>(23-June-2008)</i> <b>wxPython 2.8.8.0</b>  This release had a number of
+further refinements and enhancements on the stable 2.8 source tree
+since the previous release.   On Mac OS X 10.5 (Leopard) the Python
+2.5 binaries of wxPython are able to be used with either Apple's
+system Python, or with the Python.org version. <p>
+
+<!--    
+<li><i>(29-Nov-2007)</i> <b>wxPython 2.8.7.1</b>  This release has had some bugs
+fixed, some minor patches applied, and also incorporates the Google
+Summer of Code 2007 version of XRCed, and adds the Editra source code
+editor.
+<p>
+<b>NOTE</b>: On Mac OS X 10.5 (Leopard) the Python 2.5 binaries of wxPython expect to be used with the user-installed version of <a href="http://python.org/download/">MacPython</a>, <b><i>not</i></b> the Apple installed version. A fix for this issue is being worked on for the next release. In the meantime you can either install MacPython 2.5.1 and adjust your paths so that that Python is used, or you can stick with Apple's Python and the wxPython 2.8.4.0 that comes with Leopard.
+
+<p><li><i>(26-Oct-2007)</i> <b>wxPython 2.8.6.1</b>  This release has some important bug fixes and is a general improvement over 2.8.6.0.
+
+<p><li><i>(27-Sept-2007)</i> <b>wxPython 2.8.6.0</b>  This release is mostly about fixing a number of bugs and inconsistencies in wxWidgets and wxPython.  
+
+     
+<p><li><i>(13-Sept-2007)</i> The <b>mail lists are up</b> and are working on processing all the queued messages.  This was definitely a "when it rains it pours" kind of situation, see the <a href="http://dotsrc.org/news/">DotSrc news archive</a> for more details.
+
+<p><li><i>(10-Sept-2007)</i> Yes, the <b>mail lists are down</b> and have been since the 8th.  There were multiple disk failures in RAID5 array of the machine that <a href="http://dotsrc.org/">hosts</a> the mail lists.  The hardware has been replaced and data is being restored from tape backups.
+     
+<p><li><i>(8-Aug-2007)</i> <b>wxPython 2.8.4.2</b> This release includes several fixes and a few additions and improvements, as well as new build features for <a href="http://wiki.wxpython.org/InstallingOnUbuntuOrDebian">Debian/Ubuntu</a> and OS X.
+  
+<p><li><i>(14-May-2007)</i> <b>wxPython 2.8.4.0</b>  lots of updates, fixes and improvements.
+
+<p><li><i>(22-Mar-2007)</i> <b>wxPython 2.8.3.0</b> This release includes a number of bug fixes and also some new enhancements, including updates to the XRCed tool and the new InspectionTool.  
+
+<p><li><i>(20-Jan-2007)</i> <b>wxPython 2.8.1.1</b>  This release adds a few minor enhancements and a number of bug fixes designed to further stabalize
+the 2.8.x release series.
+
+ 
+<p><li><i>(11-Dec-2006)</i> The wxPython reference docs have been put on a server that doesn't choke on large files like this one does.  You can now access the <a href="http://wxpython.wxcommunity.com/docs/api/"><b>API docs</b></a> at <a href="http://wxcommunity.com/">wxCommunity</a>.  Please update your bookmarks.  As always you can also <a href="download.php#documentation">download a tarball</a> containing the html files to access locally if you wish.
+
+<p><li><i>(11-Dec-2006)</i> <b>wxPython 2.8.0.1</b> has been released.  This is the first in a new <a href="http://wiki.wxpython.org/index.cgi/ReleaseSeries">stable release series</a>, and includes a few new widget classes and other enhancements since 2.7.2.  
+
+<p><li><i>(7-Nov-2006)</i> <b>wxPython 2.7.2.0</b> has been released.  This is expected to be the last stepping stone in the path to the next stable release series, 2.8.x.  We're driving full speed ahead in order to get 2.8.0 included with OSX 10.5, and so far we are very close to being on schedule.  This release has some house-keeping style changes, as well as some user-contributed patches and also the usual crop of bug fixes.  
+
+<p><li><i>(26-Oct-2006)</i> <b>wxPython 2.7.1.3</b> has been released.  This is another quick turn-around bug-fix release. 
+
+<p><li><i>(21-Oct-2006)</i> <b>wxPython 2.7.1.2</b> has been released.  This is bug-fix release meant to resolve some issues discovered in the 2.7.1.1 release.
+
+<p><li><i>(18-Oct-2006)</i> <b>wxPython 2.7.1.1</b> has been released.  This is the first official release of the 2.7 development branch.  Lots of new features have been added, and some existing things have changed (but mostly in backwards compatible ways.) 
+
+<p><li><i>(15-July-2006)</i> <b>wxPython 2.6.3.3</b> has been <a href="download-2.6.3.3.php">released</a>. This is mostly a bugfix release.  
+
+<p><li>A snapshot of the new style wxPython reference docs is available <a href="docs/api/">here</a>.  While most of the content is not yet present, the docs are still usable, and in fact helpful since they already accurately document what classes and methods are present in wxPython and what the parameter names are.
+
+<p><li>Robin's OSCON 2006 tutorial and presentation slides are <a href="presentations.php">available</a>.
+-->
+</ul>
+<p>
+</td></tr></table>
+<p>
+
+
+<br>
+<br>
+<table width="100%" border="1" cellspacing="0" bgcolor="#BCB8E7"><tr><td>
+<table width="100%" border="0" cellspacing="0" cellpadding="8"><tr><td>
+<a href="http://www.amazon.com/gp/product/1932394621?ie=UTF8&tag=wxpython-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1932394621"><img src="images/wxPyBook_cover_150.png" border="0" alt="wxPython in Action" align="left"></a>
+</td><td>&nbsp;
+</td><td valign="top">
+<font size="+2"><b>wxPython in Action</b></font>
+<br>by Noel Rappin and Robin Dunn
+<br><br>
+
+This is the first book about wxPython and offers a friendly tutorial to help you get started, a detailed guide to UI programming practices, and many samples of using wxPython to create and use user interface elements.  It covers an impressive amount of information  delivered at a measured pace, encouraging experimentation and learning by doing.
+
+<p>
+Available from:  
+<a href="http://www.manning.com/affiliate/idevaffiliate.php?id=189_23">Manning</a>,
+<a href="http://www.amazon.com/gp/product/1932394621?ie=UTF8&tag=wxpython-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1932394621">Amazon</a>, and as an <a href="http://www.manning.com/affiliate/idevaffiliate.php?id=189_23">eBook</a>.
+
+</td>
+</tr>
+</table>
+
+</tr><tr><td>
+<table width="100%" border="0" cellspacing="0" cellpadding="8"><tr><td valign="top">
+<a name="wxPyCookbook">
+<a href="http://www.amazon.com/gp/product/1849511780?ie=UTF8&tag=wxpython-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1849511780">
+<img src="images/wxPyCookbook-cover-large.jpg" alt="wxPyCookbook-cover-large" width="150" height="185" align="left"/>
+</a>
+</td><td>&nbsp;
+</td><td valign="top">
+<font size="+2"><b>wxPython 2.8 Application Development Cookbook</b></font>
+<br>by Cody Precord
+<br><br>
+
+This book includes a wealth of information on wxPython, application design, and programming approaches. The content of this book is based around wxPython 2.8 but the ideas are applicable to 2.9 and forward as well.  There are more than 80 practical recipes for developing feature-rich applications using wxPython, and plenty of example code to help you learn to quickly create robust, reliable, and reusable wxPython applications.
+
+<p>
+Available from:  <a href="https://www.packtpub.com/wxpython-2-8-application-development-cookbook/book">Packt Publishing</a>, <a href="http://www.amazon.com/gp/product/1849511780?ie=UTF8&tag=wxpython-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1849511780">Amazon</a>, and as an <a href="https://www.packtpub.com/wxpython-2-8-application-development-cookbook/book">eBook</a>.
+</td></tr>
+</table>
+</td></tr>
+</table>
+<br>
+<br>
+<br>
+
+<!--
+Work on wxPython is sponsored by:
+<p>
+<div align="center"><a href="http://www.osafoundation.org/"><img src="images/OSAFLogo.gif" width="559" height="64" alt="" border="0"></a></div>
+-->
+
+<!--
+<a href="http://www.manning.com/affiliate/idevaffiliate.php?id=189_118_1_27"><img src="http://www.manning.com/affiliate/banners/Free ebook ad 5.gif" border="0" width="631s" height="156" alt=""></a>
+-->
+
+
+</td>
+</tr>
+</table>
+
+<!-- BEGIN Footer -->
+<p><hr>
+
+<div align="center">
+<!-- <a href="http://wxPROs.com/"><img src="images/wxPROsButton.png" width="100" height="43" alt="" border="0"></a> -->
+<a href="http://wxWidgets.org/"><img src="images/wxWinButton.png" border="0" width="100" height="43" alt=""></a>
+<a href="http://wxPython.org/"><img src="images/wxPyButton.png" width="100" height="43" alt="" border="0"></a>
+<a href="http://www.wxdesigner-software.de/"><img src="images/wxDesignerButton.png" width="100" height="43" alt="" border="0"></a>
+<A href="http://sourceforge.net/projects/wxpython/"><IMG src="http://sourceforge.net/sflogo.php?group_id=10718&type=1" width="100" height="43" border="0" alt="SourceForge Logo"></A>
+</div>
+
+
+<!-- END Footer -->
+</BODY></HTML>
+
+
